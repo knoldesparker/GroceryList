816cbc2fc4f9cc1f946a1be85faec406fd87415c
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-02 10:14:31 +0100

    idk

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,2 +1,47 @@
+import java.util.*;
 public class GroceryList {
+   private String groceryName;
+   private int groceryPrice;
+   private int groceryAmount;
+   private GroceryList[] groceryList;
+
+
+    //This method will add items to the list if there is room
+    private void addItem(){
+        int i = groceryList.length;
+        int n = ++i;
+
+    }
+
+    public String getGroceryName() {
+        return groceryName;
+    }
+
+    public void setGroceryName(String groceryName) {
+        this.groceryName = groceryName;
+    }
+
+    public int getGroceryPrice() {
+        return groceryPrice;
+    }
+
+    public void setGroceryPrice(int groceryPrice) {
+        this.groceryPrice = groceryPrice;
+    }
+
+    public int getGroceryAmount() {
+        return groceryAmount;
+    }
+
+    public void setGroceryAmount(int groceryAmount) {
+        this.groceryAmount = groceryAmount;
+    }
+
+    public GroceryList[] getGroceryList() {
+        return groceryList;
+    }
+
+    public void setGroceryList(GroceryList[] groceryList) {
+        this.groceryList = groceryList;
+    }
 }

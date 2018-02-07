dee9a5b3cb28a47adc42f4b76f2d13fab47efb7e
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-07 12:54:36 +0100

    123

--- src/GroceryItemOrder.java
+++ src/GroceryItemOrder.java
@@ -1,2 +1,12 @@
 public class GroceryItemOrder {
+    private String name;
+    private double prize;
+    private int amout;
+
+    public GroceryItemOrder(String name, double prize, int amout) {
+        this.name = name;
+        this.prize = prize;
+        this.amout = amout;
+    }
 }
+
--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,4 +1,9 @@
 public class GroceryList {
+    GroceryItemOrder[] groceryItemOrders = new GroceryItemOrder[10];
+    int pointer = 0;
+
+
+
     //This method will add items to the list if there is room
     private void addItem(){
     }
